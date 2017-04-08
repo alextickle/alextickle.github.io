13c3bab4ea9814d85d978a6a9a874d8ba0898f00
Alex Tickle <alexander.tickle@gmail.com>
2017-04-07 22:55:26 -0700

    major additions to turtle.js

--- js/turtle.js
+++ js/turtle.js
@@ -1,138 +1,177 @@
-$(document).ready(function(){
-  test();
-});
+var initializeGrid = function(){
+  for (var i = 0; i < 20; i++){
+    $("#myTable").append("<tr id='row" + i + "'></tr>");
+    for (var j = 0; j < 20; j++){
+      $("#row" + i).append("<td id='" + (20 * i + j) + "' class='cell'></td>");
+    }
+  }
+  $("#210").addClass("turtle");
+}
 
 var createTurtle = function(){
-  var array = new Array(20);
-  for (var i = 0; i < 20; i++){
-    array[i] = [0];
-  }
   return {
     turtlePos: [10,10],
     penUp: false,
-    floor: array,
     direction: 0,
   }
 }
 
-var run = function() {
-  var turtle = createTurtle();
-	var exit = false;
-	var user;
-	var tempFloor = new Array(20);
-  for (var i = 0; i < 20; i++){
-    array[i] = [0];
+var processCmdList = function(string){
+  $("#instructions").text("Please enter a command (1-6):");
+  var args = string.split(" ");
+  args.forEach(function(command){
+    if (validCommand(parseInt(command))){
+      processCommand(parseInt(command));
+    }
+    else {
+      $("#instructions").text("Invalid command: Out of Bounds");
+      return;
+    }
+  });
+
+  $("#inputBox").val("");
+
+}
+
+var validCommand = function(num){
+  if (num === 5){
+    switch(turtle.direction){
+      case 0:
+        return !(turtle.turtlePos[1] > 14);
+      case 1:
+        return !(turtle.turtlePos[0] > 14);
+      case 2:
+        return !(turtle.turtlePos[1] < 4);
+      case 3:
+        return !(turtle.turtlePos[0] < 4);
+    }
+  }
+  else if (num === 6){
+    switch(turtle.direction){
+      case 0:
+        return !(turtle.turtlePos[1] > 18);
+      case 1:
+        return !(turtle.turtlePos[0] > 18);
+      case 2:
+        return !(turtle.turtlePos[1] < 1);
+      case 3:
+        return !(turtle.turtlePos[0] < 1);
+    }
+  }
+  else {
+    return true;
   }
 }
 
-var test = function(){
-  $("row:eq(" + 5 + ")").addClass("drawn");
-};
+var processCommand = function(num){
+	switch(num){
+		case 1:
+			turtle.penUp = true;
+			break;
+		case 2:
+			turtle.penUp = false;
+			break;
+		case 3:
+			if (turtle.direction === 0){
+			  turtle.direction = 3;
+			}
+			else {
+				turtle.direction -= 1;
+			}
+			break;
+		case 4:
+			if (turtle.direction === 3){
+				turtle.direction = 0;
+			}
+			else {
+				turtle.direction += 1;
+			}
+			break;
+    case 5:
+			switch (turtle.direction){
+				case 0:
+					if (turtle.penUp){
+						for (var i = 0; i <= 4; i++){
+              $("#" + ((turtle.turtlePos[1] + i) * 20  + turtle.turtlePos[0])).addClass("drawn");
+						}
+					}
+          $("#" + (turtle.turtlePos[1] * 20 + turtle.turtlePos[0])).removeClass("turtle");
+          turtle.turtlePos = [turtle.turtlePos[0], turtle.turtlePos[1] + 5];
+          $("#" + (turtle.turtlePos[1] * 20 + turtle.turtlePos[0])).addClass("turtle");
+					break;
+				case 1:
+					if (turtle.penUp){
+						for (var i = 0; i <= 4; i++){
+              $("#" + (turtle.turtlePos[1] * 20 + turtle.turtlePos[0] + i)).addClass("drawn");
+						}
+          }
+          turtle.turtlePos = [turtle.turtlePos[0] + 5, turtle.turtlePos[1]];
+          $("#" + (turtle.turtlePos[1] * 20 + turtle.turtlePos[0])).addClass("turtle");
+					break;
+				case 2:
+          if (turtle.penUp){
+            for (var i = 0; i <= 4; i++){
+              $("#" + ((turtle.turtlePos[1] - i) * 20 + turtle.turtlePos[0])).addClass("drawn");
+            }
+          }
+          turtle.turtlePos = [turtle.turtlePos[0], turtle.turtlePos[1] - 5];
+          $("#" + (turtle.turtlePos[1] * 20 + turtle.turtlePos[0])).addClass("turtle");
+				break;
+				case 3:
+          if (turtle.penUp){
+            for (var i = 0; i <= 4; i++){
+              $("#" + (turtle.turtlePos[1] * 20 + turtle.turtlePos[0] - i)).addClass("drawn");
+            }
+          }
+          $("#" + (turtle.turtlePos[1] * 20 + turtle.turtlePos[0])).removeClass("turtle");
+          turtle.turtlePos = [turtle.turtlePos[0] - 5, turtle.turtlePos[1]];
+          $("#" + (turtle.turtlePos[1] * 20 + turtle.turtlePos[0])).addClass("turtle");
+          break;
+			}
+			break;
+    case 6:
+      switch (turtle.direction){
+        case 0:
+          if (turtle.penUp){
+            $("#" + ((turtle.turtlePos[1]) * 20  + turtle.turtlePos[0])).addClass("drawn");
+          }
+          $("#" + (turtle.turtlePos[1] * 20 + turtle.turtlePos[0])).removeClass("turtle");
+          turtle.turtlePos = [turtle.turtlePos[0], turtle.turtlePos[1] + 1];
+          $("#" + (turtle.turtlePos[1] * 20 + turtle.turtlePos[0])).addClass("turtle");
+          break;
+        case 1:
+          if (turtle.penUp){
+            $("#" + ((turtle.turtlePos[1]) * 20  + turtle.turtlePos[0])).addClass("drawn");
+          }
+          turtle.turtlePos = [turtle.turtlePos[0] + 1, turtle.turtlePos[1]];
+          $("#" + (turtle.turtlePos[1] * 20 + turtle.turtlePos[0])).addClass("turtle");
+          break;
+        case 2:
+          if (turtle.penUp){
+            $("#" + ((turtle.turtlePos[1]) * 20  + turtle.turtlePos[0])).addClass("drawn");
+          }
+          turtle.turtlePos = [turtle.turtlePos[0], turtle.turtlePos[1] - 1];
+          $("#" + (turtle.turtlePos[1] * 20 + turtle.turtlePos[0])).addClass("turtle");
+        break;
+        case 3:
+          if (turtle.penUp){
+            $("#" + ((turtle.turtlePos[1]) * 20  + turtle.turtlePos[0])).addClass("drawn");
+          }
+          $("#" + (turtle.turtlePos[1] * 20 + turtle.turtlePos[0])).removeClass("turtle");
+          turtle.turtlePos = [turtle.turtlePos[0] - 1, turtle.turtlePos[1]];
+          $("#" + (turtle.turtlePos[1] * 20 + turtle.turtlePos[0])).addClass("turtle");
+          break;
+      }
+      break;
+	}
+}
 
-// function processCommand(string){
-// 		user = parseInt(string);
-// 		switch(user){
-// 			case 1:
-// 				turtle.penUp = true;
-// 				break;
-// 			case 2:
-// 				turtle.penUp = false;
-// 				break;
-// 			case 3:
-// 				if (turtle.direction === 0){
-// 				  turtle.direction = 3;
-// 				}
-// 				else {
-// 					turtle.direction -= 1;
-// 				}
-// 				break;
-// 			case 4:
-// 				if (turtle.direction === 3){
-// 					turtle.direction = 0;
-// 				}
-// 				else {
-// 					turtle.direction += 1;
-// 				}
-// 				break;
-// 				switch (turtle.direction){
-// 					case 0:
-// 						if (turtle.penUp){
-// 							tempFloor = turtle.floor;
-// 							for (int i = turtle.turtlePos[0]; i < turtle.turtlePos[0] - 5; i++){
-//                 $("row:eq(" + i + ") td:eq(" + turtle.turtlePos[0] + ")").addClass("drawn");
-// 							}
-// 							tempFloor[t.getTurtlePos()[0] - 4][t.getTurtlePos()[1]] = 2;
-// 							t.setFloor(tempFloor);
-// 							t.setTurtlePos(t.getTurtlePos()[0] - 4, t.getTurtlePos()[1]);
-// 						}
-//
-// 						else{
-// 							tempFloor[t.getTurtlePos()[0] - 4][t.getTurtlePos()[1]] = 2;
-// 							t.setFloor(tempFloor);
-// 							t.setTurtlePos(t.getTurtlePos()[0] - 4, t.getTurtlePos()[1]);
-// 						}
-// 						break;
-// 					case 1:
-// 						if (turtle.penUp){
-// 							tempFloor = turtle.floor;
-// 							for (int i = t.getTurtlePos()[1]; i < t.getTurtlePos()[1] - 5; i++){
-// 								tempFloor[t.getTurtlePos()[0]][i] = 1;
-// 							}
-// 							tempFloor[t.getTurtlePos()[0]][t.getTurtlePos()[1] - 4] = 2;
-// 							t.setFloor(tempFloor);
-// 							t.setTurtlePos(t.getTurtlePos()[0], t.getTurtlePos()[1] - 4);
-// 						}
-//
-// 						else{
-// 							tempFloor[t.getTurtlePos()[0]][t.getTurtlePos()[1] - 4] = 2;
-// 							t.setFloor(tempFloor);
-// 							t.setTurtlePos(t.getTurtlePos()[0], t.getTurtlePos()[1] - 4);
-// 						}
-// 						break;
-// 					case 2:
-// 						if (turtle.penUp){
-// 							tempFloor = t.getFloor();
-// 							for (int i = t.getTurtlePos()[0]; i < t.getTurtlePos()[0] + 5; i--){
-// 								tempFloor[i][t.getTurtlePos()[1]] = 1;
-// 							}
-// 							tempFloor[t.getTurtlePos()[0] + 4][t.getTurtlePos()[1]] = 2;
-// 							t.setFloor(tempFloor);
-// 							t.setTurtlePos(t.getTurtlePos()[0] + 4, t.getTurtlePos()[1]);
-// 						}
-//
-// 						else{
-// 							tempFloor[t.getTurtlePos()[0] + 4][t.getTurtlePos()[1]] = 2;
-// 							t.setFloor(tempFloor);
-// 							t.setTurtlePos(t.getTurtlePos()[0] + 4, t.getTurtlePos()[1]);
-// 						}
-// 						break;
-// 					case 3:
-// 						if (turtle.penUp){
-// 							tempFloor = t.getFloor();
-// 							for (int i = t.getTurtlePos()[1]; i < t.getTurtlePos()[1] + 5; i++){
-// 								tempFloor[t.getTurtlePos()[0]][i] = 1;
-// 							}
-// 							tempFloor[t.getTurtlePos()[0]][t.getTurtlePos()[1] + 4] = 2;
-// 							t.setFloor(tempFloor);
-// 							t.setTurtlePos(t.getTurtlePos()[0], t.getTurtlePos()[1] + 4);
-// 						}
-//
-// 						else{
-// 							tempFloor[t.getTurtlePos()[0]][t.getTurtlePos()[1] + 4] = 2;
-// 							t.setFloor(tempFloor);
-// 							t.setTurtlePos(t.getTurtlePos()[0], t.getTurtlePos()[1] + 4);
-// 						}
-// 						break;
-// 				}
-// 				break;
-// 			case 6:
-// 				displayArray(t.getFloor());
-// 				break;
-// 			case 7:
-// 				printCommands();
-// 				break;
-// 			case 9:
-// 				exit = true;
-// 				break;
-// 		}
-// }
+
+var turtle = createTurtle();
+
+$(document).ready(function(){
+  initializeGrid();
+  $("#input-btn").click(function(){
+      processCmdList($("#inputBox").val());
+  });
+});
